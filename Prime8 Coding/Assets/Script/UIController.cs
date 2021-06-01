7909c2d78b34d1aade03c84cc4482af9e5cc9b88
Joseph Asante <thommpson19@gmail.com>
2021-06-01 20:19:02 +0000

    UI and UX Design Update
    UI design and UI implementation of Script, Costumes, Sound buttons, Character Library, Background Library

---
+++ Prime8 Coding/Assets/Script/UIController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject ScriptFrame;
+    public GameObject CostumeFrame;
+    public GameObject SoundsFrame;
+    public GameObject CharacterLibrary;
+    public GameObject BackgroundLibrary;
+
+    public void ScriptFrameOn()
+    {
+        ScriptFrame.SetActive(true);
+    }
+    public void ScirptFrameOff()
+    {
+        ScriptFrame.SetActive(false);
+    }
+    public void CostumeFrameOn()
+    {
+        CostumeFrame.SetActive(true);
+    }
+    public void CostumeFrameOff()
+    {
+        CostumeFrame.SetActive(false);
+    }
+    public void SoundFrameOn()
+    {
+        SoundsFrame.SetActive(true);
+    }
+    public void SoundFrameOff()
+    {
+        SoundsFrame.SetActive(false);
+    }
+    public void CharacterLibraryOn()
+    {
+        CharacterLibrary.SetActive(true);
+    }
+    public void CharacterLibraryOff()
+    {
+        CharacterLibrary.SetActive(false);
+    }public void BackgroundLibraryOn()
+    {
+        BackgroundLibrary.SetActive(true);
+    }
+    public void BackgroundLibraryff()
+    {
+        BackgroundLibrary.SetActive(false);
+    }
+
+    
+}
